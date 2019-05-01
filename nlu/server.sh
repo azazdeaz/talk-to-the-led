baba44049ed7994cbceaf9d1b53c82c2b11071d4
Polgár András <azazdeaz@gmail.com>
2019-05-01 15:03:52 +0200

    :construction: get rasa working :tada:

--- nlu/server.sh
+++ nlu/server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 docker run \
     -p 5555:5000 \
-    -v $(pwd)/model:/app/projects \
+    -v $(pwd)/models:/app/projects \
     rasa/rasa_nlu:0.14.4-full  \
     start \
         --path /app/projects
--- nlu/train.sh
+++ nlu/train.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
+# docker run \
+#     -v $(pwd):/app/project \
+#     -v $(pwd)/models:/app/model \
+#     rasa/rasa_nlu:0.14.4-full \
+#     run \
+#         rasa train nlu \
+#             --config /app/project/nlu/config.yml \
+#             --nlu /app/project/data/nlu.md \
+#             --out /app/model
+
 docker run \
     -v $(pwd):/app/project \
-    -v $(pwd)/models:/app/model \
-    rasa/rasa_nlu:latest \
+    -v $(pwd)/models:/app/models \
+    rasa/rasa_nlu:0.14.4-full \
     run \
-        rasa train nlu \
-            --config /app/project/nlu/config.yml \
-            --nlu /app/project/data/nlu.md \
-            --out /app/model
+        python -m rasa_nlu.train \
+            -c /app/project/config.yml \
+            -d /app/project/data/nlu.md \
+            -o /app/models \
+            --project led \
+            --debug
